97e32498d1b6c56757d190b632622f4c287a5a74
steven-nguyen22 <snguy117@ucr.edu>
2023-08-20 10:02:32 -0700

    finished work experience + skills

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className="text-[#915eff]">Steven</span>
+            Hi, I'm <span className="text-[#87CEEB]">Steven</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop 3D visuals, user <br className="sm:block hidden" />{" "}
